15d3eb283b9f841ad0ff7cfcaa0bcf103bfee98f
qianshi <CN_QianShi@hotmail.com>
2023-07-29 14:07:58 +0800

    比较高性能的实现

--- slice/delete.go
+++ slice/delete.go
@@ -1,5 +1,16 @@
 package slice
 
+// RemoveAt 基础版本
 func RemoveAt(slice []int, index int) []int {
 	return append(slice[:index], slice[index+1:]...)
+}
+
+// RemoveFast 高性能版本，并增加了索引检查
+func RemoveFast(slice []int, index int) []int {
+	if index < 0 || index >= len(slice) {
+		return slice // 索引无效，直接返回原切片
+	}
+
+	copy(slice[index:], slice[index+1:])
+	return slice[:len(slice)-1]
 }
---
+++ slice/delete_test.go
@@ -0,0 +1,93 @@
+package slice_test
+
+import (
+	"github.com/okatu-loli/Go-Tools/slice"
+	"reflect"
+	"testing"
+)
+
+func TestRemoveAt(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []int
+		index    int
+		expected []int
+	}{
+		{
+			name:     "正常情况",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    2,
+			expected: []int{1, 2, 4, 5},
+		},
+		{
+			name:     "删除第一个元素",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    0,
+			expected: []int{2, 3, 4, 5},
+		},
+		{
+			name:     "删除最后一个元素",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    4,
+			expected: []int{1, 2, 3, 4},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := slice.RemoveAt(tt.slice, tt.index)
+			if !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("期望得到 %v, 但是得到 %v", tt.expected, got)
+			}
+		})
+	}
+}
+
+func TestRemoveFast(t *testing.T) {
+	tests := []struct {
+		name     string
+		slice    []int
+		index    int
+		expected []int
+	}{
+		{
+			name:     "正常情况",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    2,
+			expected: []int{1, 2, 4, 5},
+		},
+		{
+			name:     "删除第一个元素",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    0,
+			expected: []int{2, 3, 4, 5},
+		},
+		{
+			name:     "删除最后一个元素",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    4,
+			expected: []int{1, 2, 3, 4},
+		},
+		{
+			name:     "索引为负数",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    -1,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+		{
+			name:     "索引超出范围",
+			slice:    []int{1, 2, 3, 4, 5},
+			index:    10,
+			expected: []int{1, 2, 3, 4, 5},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := slice.RemoveFast(tt.slice, tt.index)
+			if !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("期望得到 %v, 但是得到 %v", tt.expected, got)
+			}
+		})
+	}
+}
