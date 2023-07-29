4f76a5cb7e42e5122b990b97f02cb8eaa80af01b
qianshi <CN_QianShi@hotmail.com>
2023-07-29 16:42:56 +0800

    泛型实现

--- slice/delete.go
+++ slice/delete.go
@@ -1,5 +1,8 @@
 package slice
 
+// Slicer 泛型切片类型
+type Slicer[T any] []T
+
 // RemoveAt 基础版本
 func RemoveAt(slice []int, index int) []int {
 	return append(slice[:index], slice[index+1:]...)
@@ -13,4 +16,16 @@ func RemoveFast(slice []int, index int) []int {
 
 	copy(slice[index:], slice[index+1:])
 	return slice[:len(slice)-1]
+}
+
+// RemoveGeneric 泛型版本
+func RemoveGeneric[T any](s Slicer[T], index int) Slicer[T] {
+	if index < 0 || index >= len(s) {
+		return s // 索引无效，直接返回原切片
+	}
+
+	// 将 Slicer[T] 转为其基础的切片类型进行操作
+	baseSlice := []T(s)
+	copy(baseSlice[index:], baseSlice[index+1:])
+	return baseSlice[:len(baseSlice)-1]
 }
